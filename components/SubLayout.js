791c311014cf07f075ee006519ae7bfa6f80eecc
syp <sy95_055@naver.com>
2022-08-03 13:20:30 +0900

    practice layout

---
+++ components/SubLayout.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function SubLayout({ children }) {
+    return (
+        <div>
+            <h1>
+                <Link href='/'>
+                    <a>Home</a>
+                </Link>
+                {children}
+            </h1>
+        </div>
+    );
+}
